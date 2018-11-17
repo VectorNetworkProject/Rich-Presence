87a5b7457f6558b8a8ed31d8b48b9f2d2fe8ca71
InkoHX <inkohx@gmail.com>
2018-11-17 14:09:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = require('discord-rich-presence')('481812773827772428')
 client.updatePresence({
   state: 'Vector Network で遊んでいます！',
   largeImageKey: 'vector_network_discord',
-	smallImageKey: 'Vector Network',
-	startTimestamp: Date.now(),
+  smallImageKey: 'Vector Network',
+  startTimestamp: Date.now(),
   instance: true
 })
